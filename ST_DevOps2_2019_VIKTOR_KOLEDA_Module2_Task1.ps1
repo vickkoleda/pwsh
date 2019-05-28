0e111ce9418f9be5930f016137cfc250a8ae91b1
vickkoleda <vick.koleda@gmail.com>
2019-05-28 15:38:00 +0300

    Add 3rd exercise

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task1.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task1.ps1
@@ -5,3 +5,5 @@ Get-Help Get-Help
 Get-Help Get-Help -Full
 #Примеры
 Get-Help Get-Help -Examples
+#3.Получите справку о новых возможностях в PowerShell 4.0 (или выше)
+Get-Help about_Windows_Powershell_5.0
