d70c4ebf27e133ba4a686e0ecbfdc46074a681dc
vickkoleda <vick.koleda@gmail.com>
2019-05-28 15:46:49 +0300

    Add 4th exercise

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task1.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task1.ps1
@@ -7,3 +7,5 @@ Get-Help Get-Help -Full
 Get-Help Get-Help -Examples
 #3.Получите справку о новых возможностях в PowerShell 4.0 (или выше)
 Get-Help about_Windows_Powershell_5.0
+#4.Получите все командлеты установки значений
+Get-Command Set-*
