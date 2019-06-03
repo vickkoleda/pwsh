21689b66f4f8344d1092e16e57788748ebf7140e
vickkoleda <vick.koleda@gmail.com>
2019-06-03 12:43:55 +0300

    3rd exercise

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task2.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task2.ps1
@@ -8,3 +8,8 @@ New-Item -Path "d:\" -Name "TestDir" -ItemType "directory"
 Rename-Item -Path "D:\TestDir" -NewName "TestDir01"
 #Удаляем каталог
 Remove-Item -Path "D:\TestDir01"
+#3.Создать папку C:\M2T2_ФАМИЛИЯ. Создать диск ассоциированный с папкой C:\M2T2_ФАМИЛИЯ.
+#Создаём папку
+New-Item -Path "C:\" -Name "M2T2_KOLEDA" -ItemType "directory"
+#Создаём диск Task2
+New-PSDrive Task2 -PSProvider FileSystem -Root "C:\M2T2_KOLEDA"
