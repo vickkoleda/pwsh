c39d99e34549b5f1a61a6f0f115109191065573c
vickkoleda <vick.koleda@gmail.com>
2019-06-03 12:35:34 +0300

    2nd exercise

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task2.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task2.ps1
@@ -1,3 +1,10 @@
 #1.Просмотреть содержимое ветви реeстра HKCU
 cd HKCU:\
 dir
+#2.Создать, переименовать, удалить каталог на локальном диске
+#Создаем каталог TestDir на диске D
+New-Item -Path "d:\" -Name "TestDir" -ItemType "directory"
+#Переименовываем каталог с TestDir На TestDir01
+Rename-Item -Path "D:\TestDir" -NewName "TestDir01"
+#Удаляем каталог
+Remove-Item -Path "D:\TestDir01"
