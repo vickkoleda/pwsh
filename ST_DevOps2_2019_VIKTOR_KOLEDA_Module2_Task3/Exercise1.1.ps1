ddc539f96fafdcd6765a6e78eb6d512b3bcba2e8
vickkoleda <vick.koleda@gmail.com>
2019-06-05 16:48:09 +0300

    Modified exercise 1.1

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/Exercise1.1.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/Exercise1.1.ps1
@@ -1,9 +1,9 @@
 #1.1.Сохранить в текстовый файл на диске список запущенных(!) служб. Просмотреть содержимое диска. Вывести содержимое файла в консоль PS.
 Param (
     [Parameter(Mandatory=$true, HelpMessage="Enter output file location. For Example D:\")]
-    [string]$Location='D:\',
+    [string]$Location,
     [Parameter(Mandatory=$true, HelpMessage="Enter output file name. For Example RunningServices.txt")]
-    [string]$File='RunningServices.txt'
+    [string]$File
 )
 Get-Service | ? {$_.Status -eq "Running"} | Out-File $Location$File
 Get-ChildItem $Location
