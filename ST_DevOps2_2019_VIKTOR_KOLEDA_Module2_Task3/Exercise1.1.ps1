729dabff84f61e95c3c1068c84f672359d7843a3
vickkoleda <vick.koleda@gmail.com>
2019-06-05 16:39:48 +0300

    Add exercise 1.1 description

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/Exercise1.1.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/Exercise1.1.ps1
@@ -1,3 +1,4 @@
+#1.1.Сохранить в текстовый файл на диске список запущенных(!) служб. Просмотреть содержимое диска. Вывести содержимое файла в консоль PS.
 Param (
     [Parameter(Mandatory=$true, HelpMessage="Enter output file location. For Example D:\")]
     [string]$Location='D:\',
