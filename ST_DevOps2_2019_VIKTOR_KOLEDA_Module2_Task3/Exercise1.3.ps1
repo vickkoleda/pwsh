79e4aaf56c07c99a9b938aa4f12cf3688f18b02d
vickkoleda <vick.koleda@gmail.com>
2019-06-06 10:30:51 +0300

    Add exercise 1.3

---
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/Exercise1.3.ps1
@@ -0,0 +1,8 @@
+#1.3.Вывести список из 10 процессов занимающих дольше всего процессор. Результат записывать в файл.
+Param (
+    [Parameter(Mandatory=$true, HelpMessage="Enter output file location. For Example D:\")]
+    [string]$Location='D:\',
+    [Parameter(Mandatory=$true, HelpMessage="Enter output file name. For Example Processes.txt")]
+    [string]$File='Processes.txt'
+)
+Get-Process | Sort-Object -Descending CPU | Select -First 10 | Out-File $Location$File
