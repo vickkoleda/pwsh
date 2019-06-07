811943ce60d75af18a2b1ac45ea9786b992f5efd
vickkoleda <vick.koleda@gmail.com>
2019-06-07 09:54:27 +0300

    With Exercise 2

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3/ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task3.ps1
@@ -94,3 +94,20 @@ Import-Clixml $TargetDir$XmlFile.xml |
                 Write-Host -ForegroundColor Blue "PSProvider = "$_.PSProvider
                 Write-Host "----------"
             }
+
+#.\Exercise2.ps1
+#2.1.Создать профиль
+New-Item -ItemType file -Path $profile -force
+#2.2.В профиле изненить цвета в консоли PowerShell
+echo '(Get-Host).UI.RawUI.ForegroundColor = "yellow"' >> C:\Users\Admin\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+echo '(Get-Host).UI.RawUI.BackgroundColor = "grey"' >> C:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#2.3.Создать несколько собственный алиасов
+echo "Set-Alias -Name getcom -Value Get-Command" >> c:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+echo "Set-Alias -Name pwsh -Value Get-PSVersion" >> c:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#2.4.Создать несколько констант
+echo "`$pi=3.14" >> c:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+echo "`$e=2.718" >> c:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#2.5.Изменить текущую папку
+echo "Set-Location D:" >> c:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#2.6.Вывести приветсвие
+echo 'Write-Host "Are you ready to script?"' >> c:\Users\vkaliada\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
