5fed801c57357b4a02be5a46b6b937c71bc148fb
vickkoleda <vick.koleda@gmail.com>
2019-06-12 00:20:48 +0300

    Several changes

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task5.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task5.ps1
@@ -1,9 +1,11 @@
 #1.При помощи WMI перезагрузить все виртуальные машины.
-ForEach-Object($Vm in (Get-WmiObject -Namespace root\virtualization\v2 Msvm_ComputerSystem | Where-Object {$_.Description -like "Microsoft Virtual Computer System"}) {
-$Vm.RequestStateChange(10)
+$VMs=Get-WmiObject -Namespace root\virtualization\v2 -Query "SELECT * FROM Msvm_ComputerSystem WHERE Caption = 'Virtual Machine'" 
+ForEach ($a in $VMs) {
+    Write-Output "VM $($a.ElementName) is now will restart"
+    $a.RequestStateChange(10)
 }
 #2.При помощи WMI просмотреть список запущенных служб на удаленном компьютере
-Get-WmiObject win32_service -Credential koleda_vm3\administrator -Computer 192.168.10.3
+Get-WmiObject win32_service -Credential koleda_vm2\administrator -Computer 192.168.10.100
 #3.Настроить PowerShell Remoting, для управления всеми виртуальными машинами с хостовой
 #На каждой виртуальной машине прописываем
 Enable-PSremoting
@@ -12,4 +14,7 @@ Set-Service WinRM -startuptype Automatic
 winrm set winrm/config/client ‘@{TrustedHosts="*"}’
 #4.Для одной из виртуальных машин установить для прослушивания порт 42658. 
 Set-Item WSMan:\localhost\listener\*\Port 42658
+Restart-Service winrm
+netsh advfirewall firewall add rule name="Win rm port 42658" dir=in action=allow protocol=TCP localport=42658
+Restart-Service winrm
 #5.Создать конфигурацию сессии с целью ограничения использования всех команд, кроме просмотра содержимого дисков.
