64c0dc225df1415f4bf7de8280539dd0901875d4
vickkoleda <vick.koleda@gmail.com>
2019-06-14 01:43:03 +0300

    minor design changes

--- ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task6.ps1
+++ ST_DevOps2_2019_VIKTOR_KOLEDA_Module2_Task6.ps1
@@ -1,16 +1,19 @@
 #1.Для каждого пункта написать и выполнить соответсвующий скрипт автоматизации администрирования:
 #1.1.Вывести все IP адреса вашего компьютера (всех сетевых интерфейсов)
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName . | Where-Object {$_.IPAddress -ne $null} | Select-Object -Property Description, IPAddress
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName . | `
+Where-Object {$_.IPAddress -ne $null} | Select-Object -Property Description, IPAddress
 #1.2.Получить mac-адреса всех сетевых устройств вашего компьютера и удалённо.
 #MAC-адреса локального компьютера
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName . | Where-Object {$_.MACAdress -ne $null} | Select-Object -Property Description, MACAddress
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName . | `
+Where-Object {$_.MACAdress -ne $null} | Select-Object -Property Description, MACAddress
 #MAC-адреса удаленного компьютера
 param(
     [Parameter(Mandatory=$true,HelpMessage="Enter remote computer IP address or hostname")]
     [ValidateNotNull()]
     [string]$wmihost
 )
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $wmihost | Where-Object {$_.MACAddress -ne $null} | Select-Object -Property Description, MACAddress
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $wmihost | `
+Where-Object {$_.MACAddress -ne $null} | Select-Object -Property Description, MACAddress
 #1.3.На всех виртуальных компьютерах настроить (удалённо) получение адресов с DHСP.
 $VMs=Get-WmiObject -Namespace root\virtualization\v2 -Query "SELECT * FROM Msvm_ComputerSystem WHERE Caption = 'Virtual Machine'" 
 ForEach ($a in $VMs) {
@@ -18,14 +21,16 @@ ForEach ($a in $VMs) {
     $Password='Qwerty1!'
     $pass = ConvertTo-SecureString -AsPlainText $Password -Force
     $MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$pass
-    $NetAdapter=Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $a.ElementName -Credential $MySecureCreds | Where-Object {$_.Description -like "Microsoft Hyper-V Network Adapter*"}
+    $NetAdapter=Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $a.ElementName -Credential $MySecureCreds | `
+    Where-Object {$_.Description -like "Microsoft Hyper-V Network Adapter*"}
     foreach ($i in $NetAdapter) {
         $i.InvokeMethod("EnableDHCP", $null)
     }
 }
 #1.4.Расшарить папку на компьютере
 New-Item e:\sharedfolder -Type dir
-(Get-WmiObject -List -ComputerName . | Where-Object -Filter {$_.Name -eq "win32_Share"}).InvokeMethod("Create",("e:\sharedfolder","TestShare",0,15,"Test Share"))
+(Get-WmiObject -List -ComputerName . | `
+Where-Object -Filter {$_.Name -eq "win32_Share"}).InvokeMethod("Create",("e:\sharedfolder","TestShare",0,15,"Test Share"))
 #1.5.Удалить шару из п.1.4
 (Get-WmiObject win32_share -ComputerName . | Where-Object {$_.name -eq "TestShare"}).InvokeMethod("Delete",$null)
 #1.6.Скрипт входными параметрами которого являются Маска подсети и два ip-адреса. Результат  – сообщение (ответ) в одной ли подсети эти адреса.
@@ -339,7 +344,8 @@ Name  State CPUUsage(%) MemoryAssigned(M) Uptime   Status             Version
 ----  ----- ----------- ----------------- ------   ------             -------
 NewVM Off   0           0                 00:00:00 Operating normally 9.0
 #>
-#Проверяем существует ли network adapter. Если да, то подключаем его к private сети, если нет, то создаём новый и подключаем его к private сети 
+#Проверяем существует ли network adapter. Если да, то подключаем его к private сети, 
+#если нет, то создаём новый и подключаем его к private сети 
 if (((Get-VM -Name NewVM).NetworkAdapters).id -ne $null) {
     Connect-VMNetworkAdapter -VMName NewVM -SwitchName "Private Virtual Network"
 }
